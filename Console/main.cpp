18faad94b6e3957665112432f8b20255fca36dbe
SaiDhinakar <saidhinakars899@gmail.com>
2025-05-15 16:12:34 +0530

    CRUD operations added

--- Console/main.cpp
+++ Console/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <map>
 #include <iomanip>
+#include <chrono>
+#include <thread>
 
 using namespace std;
+
+void timer(int seconds) {
+    this_thread::sleep_for(chrono::seconds(seconds));
+    system("clear");
+}
 
 class PasswordManager {
     
@@ -21,9 +28,39 @@ class PasswordManager {
         void addPassword(string name, string password){
             if(!find_matching(name, password)){
                 passwords[name] = password;
+                cout<<"Password addded successfully"<<std::flush;
+                timer(2);
             }
             else{
-                cout<<"The name or password of the app alreay exists";
+                system("clear");
+                cout<<"The name or password of the app alreay exists"<<std::flush;
+                timer(2);
+            }
+        }
+
+        void updatePassword(string name, string password, string newPassword){
+            if(find_matching(name, password)){
+                passwords[name] = newPassword;
+                cout<<"Password updated successfully..."<<std::flush;
+                timer(2);
+            }
+            else{
+                system("clear");
+                cout<<"The name or password of the app not exists"<<std::flush;
+                timer(2);
+            }
+        }
+
+        void removePassword(string name, string password){
+            if(find_matching(name, password)){
+                passwords.erase(name);
+                cout<<"Password Removed successfully..."<<std::flush;
+                timer(2);
+            }
+            else{
+                system("clear");
+                cout<<"The name or password of the app not exists"<<std::flush;
+                timer(2);
             }
         }
 
@@ -31,15 +68,16 @@ class PasswordManager {
             
             const int nameWidth = 20;
             const int passwordWidth = 20;
-            
             cout << left << setw(nameWidth) << "Name" << setw(passwordWidth) << "Password" << endl;
             cout << string(nameWidth + passwordWidth, '-') << endl;
             
             for(auto password : passwords){
-            cout << left << setw(nameWidth) << password.first << setw(passwordWidth) << password.second << endl;
+                cout << left << setw(nameWidth) << password.first << setw(passwordWidth) << password.second << endl;
             }
+            cout << "\nThis screen will clear in 15 seconds...\n"<<std::flush;
+            
+            timer(15);
         }
-
 };
 
 int main(){
@@ -54,6 +92,7 @@ int main(){
 
     string name, pass;
     while(true){
+        system("clear");
         cout<<"\nEnter Username : ";
         cin>>name;
         cout<<"Enter password for "+name+" : ";
@@ -73,12 +112,13 @@ int main(){
 
     if(auth){
         // Show options 
+        string newPass;
 
         do{
             cout<<"\n1.Add Password\n2.Update Password\n3.Remove Password\n4.List all passwords\n5.Exit"<<endl;
             cout<<"Enter the choice(Ex : 1) : ";
             cin>>ch;
-
+            system("clear");
             switch (ch)
             {
             case 1:
@@ -87,14 +127,25 @@ int main(){
                 cout<<"Enter the password : ";
                 cin>>pass;
                 PwdMg.addPassword(name, pass);
-                cout<<"Password saved successfully";
-                system("clear");
                 break;
             case 2:
+                cout<<"Enter the name of the password : ";
+                cin>>name;
+                cout<<"Enter the password : ";
+                cin>>pass;
+                cout<<"Enter the new password : ";
+                cin>>newPass;
+                PwdMg.updatePassword(name, pass, newPass);
                 break;
             case 3:
+                cout<<"Enter the name of the password : ";
+                cin>>name;
+                cout<<"Enter the password : ";
+                cin>>pass;
+                PwdMg.removePassword(name, pass);
                 break;
             case 4:
+                system("clear");
                 PwdMg.displayPasswords();
                 break;
             case 5:
